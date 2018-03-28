df7275f6b23951f8a0ba8fbf9a0b82f212643f2a
johnnyrayalt <johnnyrayalt@gmail.com>
2018-03-28 13:39:09 -0700

    JS now stores all values properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {
-  $("#form").submit(function(event) {
+  // $('#pizzaSize option').attr('selected', 'selected');
+  $('#form').submit(function(event) {
     event.preventDefault();
 
-    console.log("hi");
+    var pizzaSize = $('#pizzaSize :selected').val()
+    console.log(pizzaSize);
+    var baseOption = $('input:radio[name=sauce]:checked').val();
+    console.log(baseOption);
+    var cheeseOption = $('input:radio[name=cheese]:checked').val();
+    console.log(cheeseOption);
+    var meatOptions = []
+    $('input:checkbox[name=topping-meat]:checked').each(function() {
+      meatOptions.push($(this).val());
+    });
+    console.log(meatOptions);
+    var vegOptions = [];
+    $('input:checkbox[name=topping-veg]:checked').each(function() {
+        vegOptions.push($(this).val());
+    });
+    console.log(vegOptions);
 
   });
 });
